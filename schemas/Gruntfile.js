bb4c922f4ac3c4f2b26adce8803a440039d70374
Fred Blundun <fred@snowplowanalytics.com>
2014-05-14 15:28:04 +0100

    Renamed schemas Added tests for schema.json schema Changed regular expressions to permit hyphens Changed folder structure

---
+++ schemas/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    intern: {
+      tests: {
+        options: {
+          runType: 'client',
+          config: 'jsonschema/tests/intern.js'
+        }
+      }
+    },
+
+    json_schema: {
+      test: {
+        files: {
+          'schemas/ad_impression.json': ['inputs/ad_impression.json']
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('intern');
+
+  grunt.registerTask('default', ['intern']);
+
+}
