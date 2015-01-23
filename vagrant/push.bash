d7fe996e2fc7fb42f4544c78a4fbd065df8b58e3
Alex Dean <alex@snowplowanalytics.com>
2015-01-23 14:15:20 +0000

    Updated vagrant push command to use region and profile (closes #119)

--- vagrant/push.bash
+++ vagrant/push.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+aws_profile=snowplow
 s3_bucket=iglucentral.com
+s3_region=us-east-1
 
 # Get the parent directory of where this script is.
 source="${BASH_SOURCE[0]}"
@@ -10,4 +12,4 @@ dir="$( cd -P "$( dirname "${source}" )/.." && pwd )"
 cd ${dir}
 
 # Note - this runs on HOST: https://groups.google.com/forum/#!topic/vagrant-up/LgqE-JFAqZc 
-aws s3 cp schemas/ s3://${s3_bucket}/schemas/ --include "*.*" --recursive
+aws s3 cp schemas/ s3://${s3_bucket}/schemas/ --include "." --recursive --region=${s3_region} --profile=${aws_profile}
