15320b67886716b9e70c7e04d3a8284f4ca050b0
Anton Parkhomenko <mailbox@chuwy.me>
2020-07-22 10:15:39 +0300

    Infrastructure: bump igluctl to 0.7.1 (close #1064)

--- ci/constants.sh
+++ ci/constants.sh
@@ -1,6 +1,6 @@
 # Igluctl
 INSTALL_DIR="/tmp"
-VALIDATOR_V="0.6.0"
+VALIDATOR_V="0.7.1"
 VALIDATOR_ZIP="igluctl_${VALIDATOR_V}.zip"
 VALIDATOR_URI="http://dl.bintray.com/snowplow/snowplow-generic/${VALIDATOR_ZIP}"
 VALIDATOR_JAR="igluctl"
