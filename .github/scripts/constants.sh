99f794cbdfd14d4ff065c62f9a4fe1fb51dad1ff
Ian Streeter <ian@snowplowanalytics.com>
2022-09-07 09:26:34 +0100

    Bump igluctl version to allow schema lists in dev registry (close #1236)

--- .github/scripts/constants.sh
+++ .github/scripts/constants.sh
@@ -1,7 +1,8 @@
 # Igluctl
 INSTALL_DIR="/tmp"
-VALIDATOR_V="0.7.2"
-VALIDATOR_URI="https://github.com/snowplow-incubator/igluctl/releases/download/${VALIDATOR_V}/igluctl"
+VALIDATOR_V="0.10.1"
+VALIDATOR_ZIP="igluctl_${VALIDATOR_V}.zip"
+VALIDATOR_URI="https://github.com/snowplow-incubator/igluctl/releases/download/${VALIDATOR_V}/$VALIDATOR_ZIP"
 VALIDATOR_JAR="igluctl"
 IGLUCTL="${INSTALL_DIR}/${VALIDATOR_JAR}"
 
